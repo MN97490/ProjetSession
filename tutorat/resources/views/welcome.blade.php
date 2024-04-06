a9408f5173b6c1b6df491ae1301cba12090ce7d8
Nathan Michel <nathanalexandromichel@gmail.com>
2024-04-06 10:33:38 -0400

    update

--- tutorat/resources/views/welcome.blade.php
+++ tutorat/resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0" >
     <link rel="stylesheet" href="../style.css">
-    <title>Connexion</title>
+    <title>Connexiond</title>
     <link rel="shortcut icon" type="image/png" href="../img/apple-icon-72x72.png"/>
 </head>
 
