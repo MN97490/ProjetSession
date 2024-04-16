547a5fd2cde7d488749d5720bd4b3a0b49f01f36
Astiox <Quentin.lony1@gmail.com>
2024-04-16 09:32:39 -0400

    ajout controller message,MessagerieSeeder,route

---
+++ tutorat/app/Http/Controllers/ConversationController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ConversationController extends Controller
+{
+
+    public function index(){
+
+    }
+
+    public function show(int $id){
+
+    }
+
+}
